a7eadec803b28dd74cd242b2c27e318bf643673c
Lenard Frommelt <lenny09918050@gmail.com>
2022-11-19 11:22:46 +0300

    thingysync push

--- testing/test.mjs
+++ testing/test.mjs
@@ -1,11 +1,12 @@
 import * as sess from "../output/index.js"
+import * as secUtl from "../output/node_modules/secret-manager-crypto-utils/index.js"
 import { performance } from "perf_hooks"
 
 const results = {}
 
 const testString = "testorritestorritestorri - asdaf 456789 äö90ß´ä-`''°^"
 
-const count = 100
+const count = 10000
 
 //############################################################
 async function testAuthCode() {
@@ -14,16 +15,16 @@ async function testAuthCode() {
         var request1 = {publicKey: "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",timestamp: 0, nonce: 0, signature: "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"}
         var request2 = {authCode: "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",timestamp: 0, data:  "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"}
 
-        var kpHex = await sess.createKeyPairHex()
+        var kpHex = await secUtl.createKeyPairHex()
         var alicePrivHex = kpHex.secretKeyHex
         var alicePubHex = kpHex.publicKeyHex
 
         var context = "lenny@extensivlyon.coffee/mega-context"
 
-        var seedHex = await sess.createSharedSecretHashHex(alicePrivHex, alicePubHex, context)
+        var seedHex = await secUtl.createSharedSecretHashHex(alicePrivHex, alicePubHex, context)
         var seedBytes = Buffer.from(seedHex, "hex")
-        var authCodeHex = await sess.authCodeHex(seedHex, request1)
-        var authCodeBytes = await sess.authCodeBytes(seedBytes, request1)
+        var authCodeHex = await sess.createAuthCodeHex(seedHex, request1)
+        var authCodeBytes = await sess.createAuthCodeBytes(seedBytes, request1)
         if(authCodeHex != (Buffer.from(authCodeBytes)).toString("hex")) {
             throw new Error("Byte version and Hex version did not match!")
         }
@@ -31,6 +32,9 @@ async function testAuthCode() {
         let success = true
         let hexMS = 0
         let bytesMS = 0
+        let requestStringHexMS = 0
+        let requestStringBytesMS = 0
+        let requestString = ""
         let before = 0
         let after = 0
         let c = 0
@@ -39,7 +43,7 @@ async function testAuthCode() {
         c = count
         before = performance.now()
         while(c--) {
-            authCodeHex = sess.authCode(seedHex, request2)
+            authCodeHex = sess.createAuthCodeHex(seedHex, request2)
         }
         after = performance.now()
         hexMS = after - before
@@ -47,13 +51,31 @@ async function testAuthCode() {
         c = count
         before = performance.now()
         while(c--) {
-            authCodeBytes = sess.authCodeBytes(seedHex, request2)
+            authCodeBytes = sess.createAuthCodeBytes(seedHex, request2)
         }
         after = performance.now()
         bytesMS = after - before
 
+        requestString = JSON.stringify(request2)
 
-        results.testAuthCode= {success, hexMS, bytesMS}
+        c = count
+        before = performance.now()
+        while(c--) {
+            authCodeHex = sess.createAuthCodeHex(seedHex, requestString)
+        }
+        after = performance.now()
+        requestStringHexMS = after - before
+
+        c = count
+        before = performance.now()
+        while(c--) {
+            authCodeBytes = sess.createAuthCodeBytes(seedHex, requestString)
+        }
+        after = performance.now()
+        requestStringBytesMS = after - before
+
+
+        results.testAuthCode= {success, hexMS, bytesMS, requestStringHexMS, requestStringBytesMS}
 
     } catch(error) {
         results.testAuthCode=error.message
@@ -62,29 +84,29 @@ async function testAuthCode() {
 }
 
 //############################################################
-async function testSessionKey() {
+async function testCreateSessionKey() {
 
     try {
         var request1 = {publicKey: "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",timestamp: 0, nonce: 0, signature: "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"}
         var request2 = {authCode: "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",timestamp: 0, data:  "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"}
 
-        var kpHex = await sess.createKeyPairHex()
+        var kpHex = await secUtl.createKeyPairHex()
         var alicePrivHex = kpHex.secretKeyHex
         var alicePubHex = kpHex.publicKeyHex
 
         var context = "lenny@extensivlyon.coffee/mega-context"
 
-        var seedHex = await sess.createSharedSecretHashHex(alicePrivHex, alicePubHex, context)
+        var seedHex = await secUtl.createSharedSecretHashHex(alicePrivHex, alicePubHex, context)
         var seedBytes = Buffer.from(seedHex, "hex")
-        var sessionKeyHex = await sess.sessionKeyHex(seedHex, request1)
-        var sessionKeyBytes = await sess.sessionKeyBytes(seedBytes, request1)
+        var sessionKeyHex = await sess.createSessionKeyHex(seedHex, request1)
+        var sessionKeyBytes = await sess.createSessionKeyBytes(seedBytes, request1)
         if(sessionKeyHex != (Buffer.from(sessionKeyBytes)).toString("hex")) {
             throw new Error("Byte version and Hex version did not match!")
         }
 
-        var testCipher = await sess.symmetricEncrypt(testString, sessionKeyHex)
+        var testCipher = await secUtl.symmetricEncrypt(testString, sessionKeyHex)
         // var testUncipher = await sess.symmetricDecryptBytes(testCipher, sessionKeyBytes)
-        var testUncipher = await sess.symmetricDecrypt(testCipher, sessionKeyHex)
+        var testUncipher = await secUtl.symmetricDecrypt(testCipher, sessionKeyHex)
         
         if(testUncipher != testString) {
             throw new Error("encyption and decryption of testString did not work with our sessionKey!")
@@ -93,6 +115,9 @@ async function testSessionKey() {
         let success = true
         let hexMS = 0
         let bytesMS = 0
+        let requestStringHexMS = 0
+        let requestStringBytesMS = 0
+        let requestString = ""
         let before = 0
         let after = 0
         let c = 0
@@ -101,7 +126,7 @@ async function testSessionKey() {
         c = count
         before = performance.now()
         while(c--) {
-            sessionKeyHex = await sess.sessionKeyHex(seedHex, request2)
+            sessionKeyHex = await sess.createSessionKeyHex(seedHex, request2)
         }
         after = performance.now()
         hexMS = after - before
@@ -109,16 +134,36 @@ async function testSessionKey() {
         c = count
         before = performance.now()
         while(c--) {
-            sessionKeyBytes = await sess.sessionKeyBytes(seedBytes, request2)
+            sessionKeyBytes = await sess.createSessionKeyBytes(seedBytes, request2)
         }
         after = performance.now()
         bytesMS = after - before
 
 
-        results.testSessionKey= {success, hexMS, bytesMS}
+        requestString = JSON.stringify(request2)
+
+        c = count
+        before = performance.now()
+        while(c--) {
+            sessionKeyHex = await sess.createSessionKeyHex(seedHex, requestString)
+        }
+        after = performance.now()
+        requestStringHexMS = after - before
+
+        c = count
+        before = performance.now()
+        while(c--) {
+            sessionKeyBytes = await sess.createSessionKeyBytes(seedBytes, requestString)
+        }
+        after = performance.now()
+        requestStringBytesMS = after - before
+
+
+
+        results.testCreateSessionKey= {success, hexMS, bytesMS, requestStringHexMS, requestStringBytesMS}
 
     } catch(error) {
-        results.testSessionKey=error.message
+        results.testCreateSessionKey=error.message
     }
 
 }
@@ -127,7 +172,7 @@ async function testSessionKey() {
 async function run() {
 
     await testAuthCode()
-    await testSessionKey()
+    await testCreateSessionKey()
 
     evaluate()
 }
